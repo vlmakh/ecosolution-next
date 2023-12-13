acf509356c4ef49ca27488b785f0ae569c3fbb86
Vlad Makhnenko <vlmakh@gmail.com>
2023-12-13 07:44:53 +0200

    add images

---
+++ public/images/cases/index.js
@@ -0,0 +1,68 @@
+import mobCase1 from "./mob_case1.jpg";
+import mobCase2 from "./mob_case2.jpg";
+import mobCase3 from "./mob_case3.jpg";
+import mobCase4 from "./mob_case4.jpg";
+import mobCase5 from "./mob_case5.jpg";
+
+import tabCase1 from "./tab_case1.jpg";
+import tabCase2 from "./tab_case2.jpg";
+import tabCase3 from "./tab_case3.jpg";
+import tabCase4 from "./tab_case4.jpg";
+import tabCase5 from "./tab_case5.jpg";
+
+import dskCase1 from "./dsk_case1.jpg";
+import dskCase2 from "./dsk_case2.jpg";
+import dskCase3 from "./dsk_case3.jpg";
+import dskCase4 from "./dsk_case4.jpg";
+import dskCase5 from "./dsk_case5.jpg";
+
+// width 320 342 596
+
+export const cases = [
+  {
+    srcMob: mobCase1,
+    srcTab: tabCase1,
+    srcDsk: dskCase1,
+    alt: "Zakhidnyi Buh",
+    name: "1 Lviv Region, Radekhiv town Private Enterprise “ZAKHIDNYI BUH”",
+    tech: "Wind Power for auto field irrigation",
+    date: "July 2023",
+    width: 320,
+  },
+  {
+    srcMob: mobCase2,
+    srcTab: tabCase2,
+    srcDsk: dskCase2,
+    alt: "Bosch",
+    name: "Zhytomyr city Private Enterprise “Bosch”",
+    tech: "Solar Panels for industrial use",
+    date: "November 2023",
+  },
+  {
+    srcMob: mobCase3,
+    srcTab: tabCase3,
+    srcDsk: dskCase3,
+    alt: "Biotech",
+    name: "Rivne city Private Enterprise “Biotech”",
+    tech: "Thermal modules",
+    date: "October 2023",
+  },
+  {
+    srcMob: mobCase4,
+    srcTab: tabCase4,
+    srcDsk: dskCase4,
+    alt: "HealthyFarm",
+    name: "Kherson city Private Enterprise “HealthyFarm”",
+    tech: "Wind Power for auto field irrigation",
+    date: "September 2021",
+  },
+  {
+    srcMob: mobCase5,
+    srcTab: tabCase5,
+    srcDsk: dskCase5,
+    alt: "Biotech",
+    name: "Zaporizhia city Private Enterprise “Biotech”",
+    tech: "Mini nuclear stations",
+    date: "May 2021",
+  },
+];
