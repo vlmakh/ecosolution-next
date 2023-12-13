ea5a8a725e02fae878d69f37932647d0a45d391b
Vlad Makhnenko <vlmakh@gmail.com>
2023-12-13 07:08:47 +0200

    install dependencies

---
+++ src/data/contacts.js
@@ -0,0 +1,7 @@
+export const contacts = {
+  add: "79005, Ukraine, Lviv str. Shota Rustaveli, 7",
+  email: "office@ecosolution.com",
+  copyright: "ecosolution © 2023",
+  phone1: "38 (098) 12 34 567",
+  phone2: "38 (093) 12 34 567",
+};
