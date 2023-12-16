90934ebb7920307387ea25c122dac3cc55ababe6
Vlad Makhnenko <vlmakh@gmail.com>
2023-12-16 07:36:00 +0200

    add About

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import { Main } from "@/blocks/Main/Main";
+import { About } from "@/blocks/About/About";
 
 export default function Home() {
   return (
     <>
       <Main />
+
+      <About />
     </>
   );
 }
--- src/blocks/About/About.jsx
+++ src/blocks/About/About.jsx
@@ -1,32 +1,26 @@
-import { Section } from "../Base/Base";
-import {
-  AboutTop,
-  AboutText,
-  TitleAbout,
-  Accent,
-} from "./About.styled";
+import css from "./About.module.scss";
 
 export const About = () => {
   return (
-    <Section id="about">
-      <AboutTop>
-        <TitleAbout>
+    <section className={css.about} id="about">
+      <div className={css.about__cont}>
+        <h2 className={css.about__title}>
           LET&apos;s start with small start with ourselves
-        </TitleAbout>
+        </h2>
 
-        <AboutText>
-          <Accent>ECOSOLUTION</Accent> is the largest group of companies in
-          Western Ukraine, which is engaged in the development and
-          implementation of renewable environmentally friendly energy sources.
-          We are the first privately owned company that received a loan from the
-          European Bank for Reconstruction and Development (EBRD) and built the
-          first wind farm in Western Ukraine. During the company&apos;s work, a
-          number of projects in the field of energy were implemented. At the
-          same time, new investment projects, in particular small hydropower and
-          biofuels, are constantly being developed and developed with existing
-          objects.
-        </AboutText>
-      </AboutTop>
-    </Section>
+        <p className={css.about__text}>
+          <span className={css.accent}>ECOSOLUTION</span> is the largest group
+          of companies in Western Ukraine, which is engaged in the development
+          and implementation of renewable environmentally friendly energy
+          sources. We are the first privately owned company that received a loan
+          from the European Bank for Reconstruction and Development (EBRD) and
+          built the first wind farm in Western Ukraine. During the
+          company&apos;s work, a number of projects in the field of energy were
+          implemented. At the same time, new investment projects, in particular
+          small hydropower and biofuels, are constantly being developed and
+          developed with existing objects.
+        </p>
+      </div>
+    </section>
   );
 };
