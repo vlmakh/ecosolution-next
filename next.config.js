a0c4ec197d02ea0f97d8cdc1e28facd84e584b20
Vlad Makhnenko <vlmakh@gmail.com>
2023-12-22 08:13:37 +0200

    update Logo

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   images: {
     deviceSizes: [360, 480, 768, 1280, 1536, 2560],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
--- src/blocks/TheFooter/TheFooter.jsx
+++ src/blocks/TheFooter/TheFooter.jsx
@@ -1,19 +1,14 @@
 import { FaArrowUp, FaFacebook, FaInstagram } from "react-icons/fa";
 import { contacts } from "@/data/contacts";
-import Image from "next/image";
 import css from "./TheFooter.module.scss";
+import LogoImg from "../../../public/images/eco_logo.svg";
 
 export const TheFooter = () => {
   return (
     <footer className={css.footer}>
       <div className={css.footer__cont}>
         <div className={css.footer__logo}>
-          <Image
-            src="/images/eco_logo.png"
-            alt="ecosolution"
-            width="269"
-            height="40"
-          />
+          <LogoImg />
 
           <a className={css.arrowUp} href="#" aria-label="To the top of site">
             <FaArrowUp size="16" />
